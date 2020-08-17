7eb8434963e6b30c3ce3f1332cbc2389d942c478
EgimenoS <ergisan@gmail.com>
2020-08-17 20:31:44 +0200

    creados componentes esenciales

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Nav } from "./components/Nav";
+import { MovieGrid } from "./components/MovieGrid";
 
 function App() {
-  return <div className="App">Movies</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <MovieGrid />
+    </div>
+  );
 }
 
 export default App;
